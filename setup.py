3ddb2ae85073950dfd0ac993e197d1f87c84a854
raphaeltimbo <raphaelts@gmail.com>
2016-10-28 17:24:10 -0200

    tests

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='LaviRot',
+      version='1.0',
+      description='LaviRot',
+      author='GLavi',
+      author_email='raphaelts@gmail.com',
+      packages=['numpy', 'scipy', 'matplotlib'],
+     )
