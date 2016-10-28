5694b67c955e69df9b56d7b97d93b15f64027276
raphaeltimbo <raphaelts@gmail.com>
2016-10-28 17:38:21 -0200

    tests

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 
 setup(name='LaviRot',
       version='1.0',
       description='LaviRot',
       author='GLavi',
       author_email='raphaelts@gmail.com',
-      packages=['numpy', 'scipy', 'matplotlib'],
+      packages=['LaviRot'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
      )
