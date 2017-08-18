f9c0b7333326ab1d49c3b2eb3f6526dd8e9c1eb1
raphael <raphaelts@gmail.com>
2017-08-18 11:01:30 -0300

    Added repr str methods to materials class.

--- LaviRot/materials.py
+++ LaviRot/materials.py
@@ -4,15 +4,21 @@ This module defines the Material class and defines
 some of the most common materials used in rotors.
 """
 
+__all__ = ['Material']
+
 
 class Material:
     """Material.
 
-    Class used to create a material and define its properties
+    Class used to create a material and define its properties.
+    Density and at least at least 2 arguments from E, G_s and
+    Poisson should be provided.
 
-    Attributes
+    Parameters
     ----------
 
+    name : str
+        Material name.
     E : float
         Young's modulus.
     G_s : float
@@ -20,13 +26,19 @@ class Material:
     rho : float
         Density.
     """
-    def __init__(self, rho=None, E=None, G_s=None, Poisson=None):
+    def __init__(self, name=None, rho=None, E=None, G_s=None, Poisson=None):
         if rho is None:
             raise ValueError('Density (rho) not provided.')
-        if tuple(locals().values()).count(None) > 1:
+
+        none_args = []
+        for arg in ['E', 'G_s', 'Poisson']:
+            if locals()[arg] is None:
+                none_args.append(arg)
+        if len(none_args) > 1:
             raise ValueError('At least 2 arguments from E, G_s'
                              'and Poisson should be provided ')
 
+        self.name = name
         self.rho = rho
         self.E = E
         self.G_s = G_s
@@ -37,8 +49,20 @@ class Material:
             self.G_s = E/(2*(1 + Poisson))
         elif Poisson is None:
             self.Poisson = (E/(2*G_s)) - 1
-        # TODO If E is None
-        # TODO Implement tests
 
         self.color = None  # this can be used in the plots
 
+    def __repr__(self):
+        return f'{self.name}'
+
+    def __str__(self):
+        return (
+            f'{self.name}'
+            f'\n{35*"-"}'
+            f'\nDensity         (N/m**3): {float(self.rho):{2}.{8}}'
+            f'\nYoung`s modulus (N/m**2): {float(self.E):{2}.{8}}'
+            f'\nShear modulus   (N/m**2): {float(self.G_s):{2}.{8}}'
+            f'\nPoisson coefficient     : {float(self.Poisson):{2}.{8}}')
+
+
+AISI4140 = Material(name='AISI4140', rho=785, E=203.2e9, G_s=80e9)
